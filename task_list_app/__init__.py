f3d31cbf872bcdd5d78c5da94da0e9f695aac0b0
GavrilS <gavr1lll@abv.bg>
2024-11-24 17:35:09 +0200

    Escape HTML to avoid ingestion attacks

--- task_list_app/__init__.py
+++ task_list_app/__init__.py
@@ -1,6 +1,7 @@
 from flask import (
     Flask, render_template, url_for
 )
+from markupsafe import escape
 
 def create_app():
     app = Flask(__name__)
@@ -10,5 +11,10 @@ def create_app():
     def hello():
         # return 'Hello World!'
         return render_template('greeting/hello.html')
+    
+    # Escaping HTML
+    @app.route('/<name>')
+    def greet_person(name):
+        return f"Hello, {escape(name)}"
 
     return app
