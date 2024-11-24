8dbbc4d4846d4615ed783dfe79a54f4596885672
GavrilS <gavr1lll@abv.bg>
2024-11-24 18:40:43 +0200

    Test request context and build URLs with url_for

--- task_list_app/__init__.py
+++ task_list_app/__init__.py
@@ -6,6 +6,7 @@ from markupsafe import escape
 def create_app():
     app = Flask(__name__)
 
+    # Hidden urls -> having some fun with the users
     # a simple page that says hello
     @app.route('/hello')
     def hello():
@@ -16,5 +17,11 @@ def create_app():
     @app.route('/<name>')
     def greet_person(name):
         return f"Hello, {escape(name)}"
+    # End of HUs
+
+    with app.test_request_context():
+        print(url_for('hello'))
+        print(url_for('hello', query_param='!', next='/'))
+        print(url_for('greet_person', name='John Doe'))
 
     return app
