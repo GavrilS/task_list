14bade81cb1b8f0ff6aeeacd1dfe1865b6b30c4e
GavrilS <gavr1lll@abv.bg>
2024-11-30 20:51:29 +0200

    Updated environment setup script to include database required libraries for the project

--- setup_env.sh
+++ setup_env.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+rm -rf env/
 python3 -m venv env
 source env/bin/activate
-python3 -m pip install Flask
+python3 -m pip install Flask SQLAlchemy mysql-connector-python
 python3 -m pip freeze > requirements.txt
 deactivate
