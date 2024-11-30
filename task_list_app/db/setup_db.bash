cbf7f9638cece3981d3bdbb2917342367b166ce8
GavrilS <gavr1lll@abv.bg>
2024-11-30 20:09:25 +0200

    Updated the bash script to include the option to drop the database and recreate it

--- task_list_app/db/setup_db.bash
+++ task_list_app/db/setup_db.bash
@@ -3,13 +3,19 @@
 echo "Provide mysql username"
 read user
 
-# echo "Provide mysql user password"
-# read password
+echo "Provide mysql user password"
+read password
 
-# if [[ "$#" -ge 1 ]] && [[ $1 = "drop_db" ]]
-# then
-#     echo "Dropping database"
-#     sudo mysql -u $user -p $password drop database if exists task_list;
-# fi
+if [[ "$#" -ge 1 ]] && [[ $1 = "drop_db" ]]
+then
+    echo "DROPPING DATABASE 'task_list'"
+    echo "===================="
+    sudo mysql -u $user -p $password <<EOF
+    drop database if exists task_list;
+    show databases;
+EOF
+    echo "DROPPED DATABASE. RECREATING A NEW DATABASE."
+    echo "===================="
+fi
 
-sudo mysql -u $user -p < ./setup_db_structure.sql
+sudo mysql -u $user -p $password < ./setup_db_structure.sql
