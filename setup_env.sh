dfadb02327bca90ecf0a200fd2a5ad57331a0119
GavrilS <gavr1lll@abv.bg>
2024-12-17 02:10:15 +0200

    Restructuring - moving the database setup files to the main dir and updating the setup env script to install libraries from the requirements file

--- setup_env.sh
+++ setup_env.sh
@@ -3,6 +3,7 @@
 rm -rf env/
 python3 -m venv env
 source env/bin/activate
-python3 -m pip install Flask SQLAlchemy mysql-connector-python
-python3 -m pip freeze > requirements.txt
+# python3 -m pip install Flask SQLAlchemy mysql-connector-python
+# python3 -m pip freeze > requirements.txt
+python3 -m pip install -r requirements.txt
 deactivate
