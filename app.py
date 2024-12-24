2904c36700e12bf371f5d0e65b3f083a94e2df67
GavrilS <gavr1lll@abv.bg>
2024-12-25 01:07:49 +0200

    Added secret key to the app so the session can work

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
+import uuid
 from task_list_app import create_app, auth
 
 def main():
     app = create_app()
 
     app.register_blueprint(auth.bp)
+
+    secret_uuid = uuid.uuid4()
+    app.secret_key = secret_uuid
 
     app.run(debug=True)
 
